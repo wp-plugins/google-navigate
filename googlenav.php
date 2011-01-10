3dd1f7867510f9d25b82bd831420e47e13ed5b02
dopert3 <dopert3@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-10 14:37:22 +0000

    added de scripts :( stupid me
    git-svn-id: https://plugins.svn.wordpress.org/google-navigate/tags/4.3@330781 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- googlenav.php
+++ googlenav.php
@@ -3,7 +3,7 @@
 Plugin Name: Google Navigator
 Plugin URI: http://googlenavigator.heefthetgemaakt.nl
 Description: a plugin for a contact page. add the adres you want visitor to navigate to. Visitors type their address. They get direct driving directions in the browser's default.
-Version: 1.1
+Version: 1.1.1
 Author: Robert van Bekkum
 Author URI: http://www.bekcomp.nl
 License: A "Slug" license name e.g. GPL2
@@ -32,7 +32,7 @@ function mt_add_pages() {
 
 function add_googlenav_header(){
   echo "";
-  $dir = WP_PLUGIN_URL.'/GoogleNav/scripts/';
+  $dir = WP_PLUGIN_URL.'/google-navigate/scripts/';
   echo <<<_end_
   <script src="{$dir}jquery.js" type="text/javascript" charset="utf-8"></script>
 	<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
